2cf30f0a589c66510977198e31088eb8ff95d3fa
stefano kitano <S.Kouroupis@gmail.com>
2017-01-12 00:04:36 +0000

    added coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,14 @@
 module.exports = function (config) {
     config.set({
         basePath: '',
-        frameworks: ['jasmine', 'source-map-support'],
+        frameworks: ['jasmine'],
         files: [
             { pattern: 'test/tsloader.js', watched: false },
         ],
         exclude: [
         ],
         preprocessors: {
-            'test/tsloader.js': ['webpack', 'coverage'],
+            'test/tsloader.js': ['webpack', 'sourcemap','coverage'],
         },
         webpack: require('./webpack.config')({ env: 'test' }),
         reporters: ['spec', 'coverage', 'remap-coverage'],
