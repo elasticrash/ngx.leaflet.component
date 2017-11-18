5d1a24d5121ffaaff1d7053f0d976f8998778e5a
unknown <s.kouroupis@gmail.com>
2017-11-18 01:00:50 +0000

    proper build

--- src/services/map.service.ts
+++ src/services/map.service.ts
@@ -56,7 +56,7 @@ export class MapService {
     }
 
     public addOverlay(overlay, name: string, gId?: string) {
-        if (gId !== undefined && this.groupIdentifiers.indexOf(gId) !== -1) {
+        if (this.groupIdentifiers.indexOf(gId) !== -1) {
             let index = this.groupIdentifiers.indexOf(gId);
             let existing_name: string = this.groupNames[index];
             this.overlays[existing_name] = overlay;
@@ -64,13 +64,13 @@ export class MapService {
             if (name === '') {
                 name = 'unknown group';
             }
-            if (gId !== undefined && this.overlays[name] === undefined) {
+            if (this.overlays[name] === undefined) {
                 this.groupNames.push(name);
                 this.groupIdentifiers.push(gId);
                 this.overlays[name] = overlay;
             } else {
                 name = this.getUniqueName(name);
-                if (gId !== undefined && this.groupNames.indexOf(name) === -1) {
+                if (this.groupNames.indexOf(name) === -1) {
                     this.groupNames.push(name);
                     this.groupIdentifiers.push(gId);
                 } else {
